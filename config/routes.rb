08618edfbd6cec77a89b6fe53426b0688b01e024
Alexandra Fren <alexandra.fren@gmail.com>
2018-11-02 18:27:03 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root 'sessions#login'
+  resources :sessions
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
